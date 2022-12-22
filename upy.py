14ecf75ec05aa659dba5f9d6ed4e1cd9871d6c07
ldscfe <ldscfe@gmail.com>
2022-12-22 16:02:41 +0800

    comment

--- upy.py
+++ upy.py
@@ -8,6 +8,7 @@ HELP = '''
   Ver        Date        Author           Description
   ---------  ----------  ---------------  ------------------------------------
   1.0        2020/11/15  Adam             Create
+  1.1        2022/12/22  Adam             udefpy2 --> udefpy3
 
 format:
     NULL
@@ -16,5 +17,5 @@ format:
 import sys
 P1=chr(117)+chr(115)+chr(114)
 P2='include'
-P3='udefpy2'
+P3='udefpy3'
 sys.path.append("/"+P1+"/"+P2+"/"+P3)
